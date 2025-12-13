9e141436bc486465c5e0232242c78ecf8dc6a186
Benjagonzmart <benjamin.gonzalez.m01@mail.pucv.cl>
2025-12-13 19:20:45 -0300

    nuevo proyecto

---
+++ autos/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+
+#clase del auto con sus respectivos atributos
+class Auto(models.Model):
+    marca = models.CharField(max_length=100)
+    modelo = models.CharField(max_length=50)
+    year = models.IntegerField()
+    precio = models.IntegerField()
+    kilometraje = models.IntegerField()
+    puertas = models.IntegerField()
+    disponible = models.BooleanField(default=True)
+
+    def __str__(self):
+        return f"{self.marca} {self.modelo} {{self.year}}"
+
+
+# Create your models here.
